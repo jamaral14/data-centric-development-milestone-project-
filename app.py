332eeedf284fd2b2ec0511168879624b9291ab37
jamaral14 <amaralmm14@hotmail.com>
2019-12-10 17:46:31 +0000

    added gitignore

--- app.py
+++ app.py
@@ -1,7 +1,11 @@
 import os
 from flask import Flask
+from flask_pymongo import PyMongo
+from bson.objectid import ObjectId 
 
 app = Flask(__name__)
+app.config["MONGO_DBNAME"] = 'handover_manager'
+app.config['MONGO_URI'] = os.getenv('MONGO_URI', 'Env value not loaded')
 
 
 @app.route('/')
@@ -12,4 +16,4 @@ def hello():
 if __name__ == '__main__':
     app.run(host=os.environ.get('IP'),
             port=int(os.environ.get('PORT')),
-            debug=True)
+                        debug=True)
