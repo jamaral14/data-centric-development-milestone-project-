2caa8300ad102a453f839e38310a7afd1afc7939
jamaral14 <amaralmm14@hotmail.com>
2019-12-10 17:01:53 +0000

    added app.run

--- app.py
+++ app.py
@@ -10,4 +10,6 @@ def hello():
 
 
 if __name__ == '__main__':
-      app.run(host='0.0.0.0', port=8000)
+    app.run(host=os.environ.get('IP'),
+            port=int(os.environ.get('PORT')),
+            debug=True)
